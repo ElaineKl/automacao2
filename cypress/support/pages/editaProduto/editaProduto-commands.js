ef87be65d7007783f85d2119a8ceb9578e09edcc
ElaineKl <elaineklug@hotmail.com>
2025-01-10 16:05:41 -0300

    Criado execução por deploy

--- cypress/support/pages/editaProduto/editaProduto-commands.js
+++ cypress/support/pages/editaProduto/editaProduto-commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add("editarProduto", (nome, valor, cores) => {
     cy.visit("http://165.227.93.41/lojinha-web/v2/produto");
-    /*cy.get('[rev="988256"] > .title > a').click()*/ 
+    cy.get ('.title').eq(0).should('be.visible').click()                                    /*('[rev="988256"] > .title > a').click()*/ 
     cy.get('#produtonome').should('be.visible').clear().type(nome);
     cy.get('#produtovalor').should('be.visible').clear().type(valor);
     cy.get('#produtocores').should('be.visible').clear().type(cores);
